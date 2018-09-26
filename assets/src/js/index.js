c93a6a18cd70a8b490609a5992234b275a79fd1c
sonnyprince <sonny.prince.sp@gmail.com>
2018-09-26 16:16:36 +0100

    v1.30.0 - update `c-badge` documentation after a11y change. additiong of fozzie stopFOIT preloading font module

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -1,10 +1,12 @@
 import '@justeat/f-footer';
 import { checkForUser } from '@justeat/f-header';
+import { stopFoit } from '@justeat/fozzie';
 import ready from 'lite-ready';
 import svg4everybody from 'svg4everybody';
 import setupValidation from './docs/formValidationSetup';
 
 ready(() => {
+    stopFoit();
     svg4everybody();
     setupValidation();
     checkForUser();
