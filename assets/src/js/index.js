ad7866dc5eb3ebd529107d60c8ece8624c28ba1b
Julian Inwood <jinw@protonmail.com>
2019-01-24 11:50:24 +0000

    Fix eslint error, disable certain styles

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -3,9 +3,9 @@ import { checkForUser } from '@justeat/f-header';
 import { stopFoit } from '@justeat/fozzie';
 import ready from 'lite-ready';
 import svg4everybody from 'svg4everybody';
+import * as utils from '@justeat/f-utilities';
 import setupValidation from './docs/formValidationSetup';
 import 'picturefill';
-import * as utils from '@justeat/f-utilities';
 
 ready(() => {
     stopFoit();
