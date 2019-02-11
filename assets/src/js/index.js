3f7095c4e0d81db8cb1b60cbdb057ba592f7ed04
Julian Inwood <jinw@protonmail.com>
2019-02-11 11:52:17 +0000

    v2.15.0 - Cookie banner (#77)
    * WH-518: Interim commit
    
    * WH-518: Add template page with menu entry
    
    * Fix eslint error, disable certain styles
    
    * Remove debug code
    
    * Add line that shouldn't have been removed
    
    * Format HTML
    
    * Update f-utils version
    
    * Update version and changelog
    
    * Remove merge tag
    
    * Tidy changelog
    
    * Update cookie page content
    
    * Remove file that should not be included

--- assets/src/js/index.js
+++ assets/src/js/index.js
@@ -3,6 +3,7 @@ import { checkForUser } from '@justeat/f-header';
 import { stopFoit } from '@justeat/fozzie';
 import ready from 'lite-ready';
 import svg4everybody from 'svg4everybody';
+import * as utils from '@justeat/f-utilities';
 import setupValidation from './docs/formValidationSetup';
 import 'picturefill';
 
@@ -11,4 +12,5 @@ ready(() => {
     svg4everybody();
     setupValidation();
     checkForUser();
+    utils.cookieBanner.init('#CookieAnchor');
 });
